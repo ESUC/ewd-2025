fcdbb9e819376aeccb4c674443cbbb8aef7bd386
Kevin Tang <kevintang2023@gmail.com>
2021-09-18 17:12:42 -0700

    EWD 2020

---
+++ .eleventy.js
@@ -0,0 +1,25 @@
+module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy("img");
+    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("js");
+    eleventyConfig.addPassthroughCopy("data");
+
+    
+    eleventyConfig.addPassthroughCopy("favicon.ico");
+    eleventyConfig.addPassthroughCopy("android-chrome-192x192.png");
+    eleventyConfig.addPassthroughCopy("android-chrome-512x512.png");
+    eleventyConfig.addPassthroughCopy("site.webmanifest");
+    
+
+    return {
+        passthroughFileCopy: true,
+        dir: {
+            input: "views",
+            output: "_site",
+        },
+        pathPrefix: "/ewd-2020/",
+        htmlTemplateEngine: "liquid",
+        markdownTemplateEngine: "liquid",
+    }
+};
