f722aa82fc6d81ded2a0be631a4a6b1c1f03e521
r-mallick <rmallick0155@gmail.com>
2023-09-13 19:01:09 -0700

    changes pathPrefix to ewd-2023

--- .eleventy.js
+++ .eleventy.js
@@ -22,7 +22,7 @@ module.exports = function(eleventyConfig) {
             output: "_site",
             includes: "_includes"
         },
-        pathPrefix: "/ewd-2022/",
+        pathPrefix: "/ewd-2023/",
         htmlTemplateEngine: "liquid",
         markdownTemplateEngine: "liquid",
     }
